1cd1a12a1fe559b17f92ef701e73562213340224
filipdyba <f.dyba28@gmail.com>
2019-03-21 17:08:15 +0100

    NEH podstawowy

--- Lab2/neh.py
+++ Lab2/neh.py
@@ -21,7 +21,7 @@ def ZnajdzNajdluzszeZadanie (M) :
             nr_indeksu = i
     return nr_indeksu
 
-def SortujNEH (M):
+def SortujPriorytetem (M):
     Kolejnosc = []
     Tmp = M[:]
     for i in range(len(Tmp)):
@@ -30,13 +30,73 @@ def SortujNEH (M):
         Kolejnosc.append(nr_indeksu+1)
     return Kolejnosc
 
+def Max (W1, W2):
+    if W1 >= W2:
+        return W1
+    else:
+        return W2
+
+def Sciezka_dochodzaca(Maszyny, Kolejnosc):
+    liczba_zadan = Maszyny[0]
+    liczba_maszyn = Maszyny[1]
+    nr_zadania = 0
+    # Dla pierwszego zadania
+    indeks = (Kolejnosc[nr_zadania]-1)*liczba_maszyn+2
+    Czas_konca = [Maszyny[indeks]]
+    for i in range(1,liczba_maszyn):
+        Czas_konca.append(Czas_konca[i-1]+Maszyny[indeks + i])
+    #Dla kolejnych zadan
+    for i in range(liczba_maszyn,liczba_zadan*liczba_maszyn):
+        if i%liczba_maszyn == 0:
+            nr_zadania += 1
+            indeks = (Kolejnosc[nr_zadania] - 1) * liczba_maszyn + 2
+            Czas_konca.append(Czas_konca[i-liczba_maszyn] + Maszyny[indeks])
+        else:
+            Czas_konca.append(Max(Czas_konca[i-1], Czas_konca[i-liczba_maszyn]) + Maszyny[i%liczba_maszyn+indeks])
+
+    return Czas_konca
+
 def Cmax(Maszyny, Kolejnosc):
-    Czas_Rozpoczecia = []
-    for i in range(Maszyny[0]):
-        Czas_Rozpoczecia[i] +=
+    sciezka = Sciezka_dochodzaca(Maszyny, Kolejnosc)
+    return sciezka[len(sciezka)-1]
+
+def NEH(Maszyny):
+    liczba_zadan = Maszyny[0]
+    liczba_maszyn = Maszyny[1]
+    Kolejnosc = []
+    Kolejnosc_naj = []
+    Cmaxmin = 9999999
+    ListaZadan = [liczba_zadan, liczba_maszyn]
+
+    Priorytety = ObliczPriorytet(Maszyny)
+    KolejnoscPriorytetow = SortujPriorytetem(Priorytety)
+    for i in range(liczba_zadan):
+        Cmaxmin = 999999
+        zadania = WezZadanie(Maszyny, KolejnoscPriorytetow[i])
+        ListaZadan.extend(zadania)
+        ListaZadan[0] = i+1 #Na liście zadań jest dokładnie i+1 zadań
+        for j in range (i+1):
+            Kolejnosc.insert(j, i+1)
+            dlugosc_trwania=Cmax(ListaZadan, Kolejnosc)
+            if dlugosc_trwania<Cmaxmin:
+                Kolejnosc_naj = Kolejnosc[:]
+                Cmaxmin = dlugosc_trwania
+            Kolejnosc.pop(j)
+        Kolejnosc = Kolejnosc_naj[:]
+    for i in range(len(Kolejnosc_naj)):
+        Kolejnosc_naj[i] = KolejnoscPriorytetow[Kolejnosc[i]-1]
+    return Cmaxmin, Kolejnosc_naj
+
+def WezZadanie (Maszyny, nr_zadania):
+    liczba_zadan=Maszyny[0]
+    liczba_maszyn = Maszyny[1]
+    tmp = []
+    indeks = (nr_zadania-1)*liczba_maszyn+2;
+    for i in range(liczba_maszyn):
+        tmp.append(Maszyny[indeks + i])
+    return tmp
 
 dane = wczytaj_plik('data.txt')
-pri = ObliczPriorytet(dane)
-print(pri)
-print(SortujNEH(pri))
-
+Cmax, KolejnoscNajlepsza = NEH(dane)
+print('Obliczony czas trwania {}'.format(Cmax))
+print('Wyznaczona kolejność {}'.format(KolejnoscNajlepsza))
--- Lab2/pomoc.py
+++ Lab2/pomoc.py
@@ -1,4 +1,5 @@
 import random
+import itertools
 
 #Wczytanie danych
 def wczytaj_plik(nazwa_pliku):
