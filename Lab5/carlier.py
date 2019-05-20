415d6eec1187c37533ee0e502739c0fd94427dc0
filipdyba <f.dyba28@gmail.com>
2019-05-21 00:48:08 +0200

    Uporzadkowany kod - carlier + 2 strategie

--- Lab5/carlier.py
+++ Lab5/carlier.py
@@ -1,20 +1,18 @@
 from schrage import *
 
 
-def carlier(permut_tasks,ub):
-    tasks = permut_tasks[:]
+def carlier(permut_tasks,ub = 999999999):
+    tasks = copy.deepcopy(permut_tasks)
     u, pi = schrage(tasks)
-    lb, O = pmtn(tasks)
+
     if u < ub:
         ub = u
-        pi_star = pi[:]
 
     a = 0
     b = 0
     c = -1
 
     #  Wyznaczenie b
-
     cpop = 0
     czas_zakonczenia_operacji = []
     for i in range(len(pi)):
@@ -22,45 +20,39 @@ def carlier(permut_tasks,ub):
         czas_zakonczenia_operacji.append(cpop)
         if cpop + pi[i][2] == u:
             b = i
-    print("b = ", b+1)
 
     #  Wyznaczenie a
-
     sum_tasks = 0
     for i in range(b, -1, -1):
         sum_tasks +=pi[i][1]
         if u == sum_tasks + pi[i][0]+pi[b][2] and czas_zakonczenia_operacji[i-1] != pi[i][1]:
             a = i
             break
-    print("a = ", a+1)
 
     #  Wyznaczenie c
-
     for i in range(a, b+1):
         if pi[i][2] < pi[b][2]:
             c = i
-    print("c = ", c+1)
 
     if c < 0:
-        return ub#, pi_star
+        return ub
 
     #  Wyznaczenie r', q' i p'
-
     pprim = 0
     rprim = pi[c+1][0]
     qprim = pi[c+1][2]
-
     for i in range(c+1, b+1):
         pprim += pi[i][1]
         if rprim > pi[i][0]:
             rprim = pi[i][0]
         if qprim > pi[i][2]:
             qprim = pi[i][2]
-    print("h(block) = ", rprim+pprim+qprim)
 
+    #Potomek po lewej stronie - zmiana czasu przygotowania zadania krytycznego
     old_pi_r_c = pi[c][0]
     pi[c][0]=max(pi[c][0], rprim+pprim)
-    print("r' = ", pi[c][0])
+
+    #Przygotowanie bloku z uwzględnieniem zadania c
     pbis = 0
     rbis = pi[c][0]
     qbis = pi[c][2]
@@ -70,15 +62,18 @@ def carlier(permut_tasks,ub):
             rbis = pi[i][0]
         if qbis > pi[i][2]:
             qbis = pi[i][2]
+
     lb, O = pmtn(pi)
     lb = max(max(rprim + qprim + pprim, rbis+pbis+qbis),lb)
-    print("Lewy potomek")
     if lb < ub:
         ub = carlier(pi, ub)
     pi[c][0] = old_pi_r_c
 
+    #Potomek po prawej stronie - zmiana czasu stygnięcia zadania krytycznego
     old_pi_q_c = pi[c][2]
     pi[c][2] = max(pi[c][2], qprim + pprim)
+
+    #Przygotowanie bloku z uwzględnieniem zadania krytycznego
     pbis = 0
     rbis = pi[c][0]
     qbis = pi[c][2]
@@ -88,14 +83,14 @@ def carlier(permut_tasks,ub):
             rbis = pi[i][0]
         if qbis > pi[i][2]:
             qbis = pi[i][2]
+
     lb, O = pmtn(pi)
     lb = max(max(rprim + qprim + pprim, rbis+pbis+qbis), lb)
-    print("Prawy potomek")
     if lb < ub:
         ub = carlier(pi, ub)
     pi[c][2] = old_pi_q_c
 
-    return ub#, pi_star
+    return ub
 
 def carlier_wideLeft(permut_tasks):
     lista_zadan = []
@@ -106,119 +101,23 @@ def carlier_wideLeft(permut_tasks):
     if u < ub:
         ub = u
 
-    a = 0
-    b = 0
-    c = -1
-
-    #  Wyznaczenie b
-    cpop = 0
-    czas_zakonczenia_operacji = []
-    for i in range(len(pi)):
-        cpop = max(cpop, max(pi[i][0],cpop) + pi[i][1])
-        czas_zakonczenia_operacji.append(cpop)
-        if cpop + pi[i][2] == u:
-            b = i
-    print("b = ", b+1)
-
-    #  Wyznaczenie a
-    sum_tasks = 0
-    for i in range(b, -1, -1):
-        sum_tasks +=pi[i][1]
-        if u == sum_tasks + pi[i][0]+pi[b][2] and czas_zakonczenia_operacji[i-1] != pi[i][1]:
-            a = i
-            break
-    print("a = ", a+1)
-
-    #  Wyznaczenie c
-    for i in range(a, b+1):
-        if pi[i][2] < pi[b][2]:
-            c = i
-    print("c = ", c+1)
-
-    if c < 0:
-        return ub
-
-    #  Wyznaczenie r', q' i p'
-    pprim = 0
-    rprim = pi[c+1][0]
-    qprim = pi[c+1][2]
-
-    for i in range(c+1, b+1):
-        pprim += pi[i][1]
-        if rprim > pi[i][0]:
-            rprim = pi[i][0]
-        if qprim > pi[i][2]:
-            qprim = pi[i][2]
-    print("h(block) = ", rprim+pprim+qprim)
-
-    old_pi_r_c = pi[c][0]
-    pi[c][0]=max(pi[c][0], rprim+pprim)
-    print("r' = ", pi[c][0])
-    pbis = 0
-    rbis = pi[c][0]
-    qbis = pi[c][2]
-    for i in range(c, b + 1):
-        pbis += pi[i][1]
-        if rbis > pi[i][0]:
-            rbis = pi[i][0]
-        if qbis > pi[i][2]:
-            qbis = pi[i][2]
-    lb, O = pmtn(pi)
-    lb = max(max(rprim + qprim + pprim, rbis+pbis+qbis),lb)
-    print("Lewy potomek")
-    if lb < ub:
-        print("dodaj Lewego")
-        kolejnosc = []
-        kolejnosc = copy.deepcopy(pi)
-        lista_zadan.append([kolejnosc, lb])
-    pi[c][0] = old_pi_r_c
-
-    old_pi_q_c = pi[c][2]
-    pi[c][2] = max(pi[c][2], qprim + pprim)
-    pbis = 0
-    rbis = pi[c][0]
-    qbis = pi[c][2]
-    for i in range(c, b + 1):
-        pbis += pi[i][1]
-        if rbis > pi[i][0]:
-            rbis = pi[i][0]
-        if qbis > pi[i][2]:
-            qbis = pi[i][2]
-    lb, O = pmtn(pi)
-    lb = max(max(rprim + qprim + pprim, rbis+pbis+qbis), lb)
-    print("Prawy potomek")
-    if lb < ub:
-        print("Dodaj prawego ", lb)
-        kolejnosc_p = []
-        kolejnosc_p = copy.deepcopy(pi)
-        lista_zadan.append([kolejnosc_p, lb])
-    print(pi[c][2], kolejnosc_p[c][2])
-    pi[c][2] = old_pi_q_c
-    print(pi[c][2], kolejnosc_p[c][2])
+    lista_zadan.append([pi, lb])
 
 
     while len(lista_zadan)!=0:
-        while len(pi)!=0:
-            pi.pop()
-        print(ub)
         lb = lista_zadan[0][1]
         podzial, O = pmtn(lista_zadan[0][0])
         if podzial > ub:
-            print("Pomijam")
             lista_zadan.pop(0)
             continue
 
         u, pi = schrage(lista_zadan[0][0])
-        print(ub)
-        print(podzial)
-
         if u < ub:
             ub = u
 
         a = 0
         b = 0
         c = -1
-
         #  Wyznaczenie b
         cpop = 0
         czas_zakonczenia_operacji = []
@@ -227,7 +126,6 @@ def carlier_wideLeft(permut_tasks):
             czas_zakonczenia_operacji.append(cpop)
             if cpop + pi[i][2] == u:
                 b = i
-        print("b = ", b + 1)
 
         #  Wyznaczenie a
         sum_tasks = 0
@@ -236,14 +134,11 @@ def carlier_wideLeft(permut_tasks):
             if u == sum_tasks + pi[i][0] + pi[b][2] and czas_zakonczenia_operacji[i - 1] != pi[i][1]:
                 a = i
                 break
-        print("a = ", a + 1)
 
         #  Wyznaczenie c
         for i in range(a, b + 1):
             if pi[i][2] < pi[b][2]:
                 c = i
-        print("c = ", c + 1)
-
         if c < 0:
             return ub
 
@@ -251,18 +146,17 @@ def carlier_wideLeft(permut_tasks):
         pprim = 0
         rprim = pi[c + 1][0]
         qprim = pi[c + 1][2]
-
         for i in range(c + 1, b + 1):
             pprim += pi[i][1]
             if rprim > pi[i][0]:
                 rprim = pi[i][0]
             if qprim > pi[i][2]:
                 qprim = pi[i][2]
-        print("h(block) = ", rprim + pprim + qprim)
 
+        #Lewy potomek
         old_pi_r_c = pi[c][0]
         pi[c][0] = max(pi[c][0], rprim + pprim)
-        print("r' = ", pi[c][0])
+
         pbis = 0
         rbis = pi[c][0]
         qbis = pi[c][2]
@@ -272,20 +166,18 @@ def carlier_wideLeft(permut_tasks):
                 rbis = pi[i][0]
             if qbis > pi[i][2]:
                 qbis = pi[i][2]
+
         lb, O = pmtn(pi)
         lb = max(max(rprim + qprim + pprim, rbis + pbis + qbis), lb)
-        print("Lewy potomek")
         if lb < ub:
-            kolejnosc = []
-            kolejnosc = copy.deepcopy(pi)
-            print(lb)
-            print(ub)
-            lista_zadan.append([kolejnosc, lb])
-            print("Dodałem lewaka")
+            kolejnosc_l = copy.deepcopy(pi)
+            lista_zadan.append([kolejnosc_l, lb])
         pi[c][0] = old_pi_r_c
 
+        #Prawy potomek
         old_pi_q_c = pi[c][2]
         pi[c][2] = max(pi[c][2], qprim + pprim)
+
         pbis = 0
         rbis = pi[c][0]
         qbis = pi[c][2]
@@ -296,36 +188,151 @@ def carlier_wideLeft(permut_tasks):
             if qbis > pi[i][2]:
                 qbis = pi[i][2]
         lb, O = pmtn(pi)
-        print(lb)
         lb = max(max(rprim + qprim + pprim, rbis + pbis + qbis), lb)
-        print("Prawy potomek")
         if lb < ub:
-            print(lb)
-            print(ub)
-            kolejnosc_p = []
             kolejnosc_p = copy.deepcopy(pi)
             lista_zadan.append([kolejnosc_p, lb])
-            print("Dodałem prawego")
         pi[c][2] = old_pi_q_c
 
-        lista_zadan.pop(0)
-        print("Długość listy = ", len(lista_zadan))
+        lista_zadan.pop(0) #Odrzuc sprawdzony wezel
+
     return ub
 
-#pliki = ["data1.txt", "data2.txt", "data3.txt", "data4.txt", "data5.txt", "data6.txt", "data7.txt", "data8.txt", "in50.txt", "in100.txt", "in200.txt"]
+def carlier_greedy(permut_tasks):
+    lista_zadan = []
+    lb = 0
+    ub = 99999999
 
-pliki = ["data1.txt"]
+    u, pi = schrage(permut_tasks)
+    if u < ub:
+        ub = u
+
+    lista_zadan.append([pi, lb])
+
+
+    while len(lista_zadan)!=0:
+        lb = lista_zadan[0][1]
+        podzial, O = pmtn(lista_zadan[0][0])
+        if podzial > ub:
+            lista_zadan.pop(0)
+            continue
+
+        u, pi = schrage(lista_zadan[0][0])
+        if u < ub:
+            ub = u
+
+        a = 0
+        b = 0
+        c = -1
+        #  Wyznaczenie b
+        cpop = 0
+        czas_zakonczenia_operacji = []
+        for i in range(len(pi)):
+            cpop = max(cpop, max(pi[i][0], cpop) + pi[i][1])
+            czas_zakonczenia_operacji.append(cpop)
+            if cpop + pi[i][2] == u:
+                b = i
+
+        #  Wyznaczenie a
+        sum_tasks = 0
+        for i in range(b, -1, -1):
+            sum_tasks += pi[i][1]
+            if u == sum_tasks + pi[i][0] + pi[b][2] and czas_zakonczenia_operacji[i - 1] != pi[i][1]:
+                a = i
+                break
+
+        #  Wyznaczenie c
+        for i in range(a, b + 1):
+            if pi[i][2] < pi[b][2]:
+                c = i
+        if c < 0:
+            return ub
+
+        #  Wyznaczenie r', q' i p'
+        pprim = 0
+        rprim = pi[c + 1][0]
+        qprim = pi[c + 1][2]
+        for i in range(c + 1, b + 1):
+            pprim += pi[i][1]
+            if rprim > pi[i][0]:
+                rprim = pi[i][0]
+            if qprim > pi[i][2]:
+                qprim = pi[i][2]
+
+        #Lewy potomek
+        old_pi_r_c = pi[c][0]
+        pi[c][0] = max(pi[c][0], rprim + pprim)
+
+        pbis = 0
+        rbis = pi[c][0]
+        qbis = pi[c][2]
+        for i in range(c, b + 1):
+            pbis += pi[i][1]
+            if rbis > pi[i][0]:
+                rbis = pi[i][0]
+            if qbis > pi[i][2]:
+                qbis = pi[i][2]
+
+        lb, O = pmtn(pi)
+        lb = max(max(rprim + qprim + pprim, rbis + pbis + qbis), lb)
+        if lb < ub:
+            kolejnosc_l = copy.deepcopy(pi)
+            lista_zadan.append([kolejnosc_l, lb])
+            #print("Lewy potomek: ub = ", ub, " lb = ", lb)
+        pi[c][0] = old_pi_r_c
+
+        #Prawy potomek
+        old_pi_q_c = pi[c][2]
+        pi[c][2] = max(pi[c][2], qprim + pprim)
+
+        pbis = 0
+        rbis = pi[c][0]
+        qbis = pi[c][2]
+        for i in range(c, b + 1):
+            pbis += pi[i][1]
+            if rbis > pi[i][0]:
+                rbis = pi[i][0]
+            if qbis > pi[i][2]:
+                qbis = pi[i][2]
+        lb, O = pmtn(pi)
+        lb = max(max(rprim + qprim + pprim, rbis + pbis + qbis), lb)
+        if lb < ub:
+            kolejnosc_p = copy.deepcopy(pi)
+            lista_zadan.append([kolejnosc_p, lb])
+            #print("Prawy potomek: ub = ", ub, " lb = ", lb)
+        pi[c][2] = old_pi_q_c
+
+        lista_zadan.pop(0) #Odrzuc sprawdzony wezel
+        lista_zadan.sort(key=operator.itemgetter(1))
+    return ub
+
+#pliki = ["in50.txt", "in100.txt", "in200.txt", "data1.txt", "data2.txt", "data3.txt", "data4.txt", "data5.txt", "data6.txt"]
+
+pliki = ["data7.txt", "data8.txt"]
 
 for i in range(len(pliki)):
     print(pliki[i])
     plik = wczytaj_plik(pliki[i])
+
     Cmax, O = schrage(plik)
-    print("Cmax Schrage: ", Cmax)
-    print(O)
-    Cmax = carlier_wideLeft(plik)#, Cmax+1)
-    print("cmax calier", Cmax)
+    print("Cmax Schrage = ", Cmax)
     Cmax, O= pmtn(plik)
-    print("Cmax pmtn: ", Cmax)
+    print("Cmax Schrage pmtn = ", Cmax)
+
+    #t0 = time.time()
+    #Cmax = carlier(plik)
+    #t1 = time.time()
+    #print("Cmax Carlier (Deep Left) = ", Cmax, " t = ", t1-t0)
+
+    t0 = time.time()
+    Cmax = carlier_wideLeft(plik)
+    t1 = time.time()
+    print("Cmax Carlier (Wide Left) = ", Cmax, " t = ", t1-t0)
+
+    t0 = time.time()
+    Cmax = carlier_greedy(plik)
+    t1 = time.time()
+    print("Cmax Carlier (Greedy) = ", Cmax, " t = ", t1-t0)
 
 
 
